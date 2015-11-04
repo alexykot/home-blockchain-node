1dd8102b907f5a321e111eeea4212de662809693
root <root@HP760-alexykot>
2015-11-04 12:28:37 +0000

    - bitcoind to bitcoinxt overhaul in progress

--- init_containers.sh
+++ init_containers.sh
@@ -2,17 +2,17 @@
 
 #build bitcoind images
 docker build --force-rm=true -t "local:base" ~/node_provision/debian-base/
-docker build --force-rm=true -t "local:bitcoind-base" ~/node_provision/bitcoind-base/
-docker build --force-rm=true -t "local:bitcoind-maindata" ~/node_provision/bitcoind-maindata/
-docker build --force-rm=true -t "local:bitcoind-mainnet" ~/node_provision/bitcoind-mainnet/
-docker build --force-rm=true -t "local:bitcoind-testdata" ~/node_provision/bitcoind-testdata/
-docker build --force-rm=true -t "local:bitcoind-testnet" ~/node_provision/bitcoind-testnet/
+docker build --force-rm=true -t "local:bitcoinxt-base" ~/node_provision/bitcoind-base/
+docker build --force-rm=true -t "local:bitcoinxt-maindata" ~/node_provision/bitcoinxt-maindata/
+docker build --force-rm=true -t "local:bitcoinxt-mainnet" ~/node_provision/bitcoinxt-mainnet/
+docker build --force-rm=true -t "local:bitcoinxt-testdata" ~/node_provision/bitcoinxt-testdata/
+docker build --force-rm=true -t "local:bitcoinxt-testnet" ~/node_provision/bitcoinxt-testnet/
 
 #start bitcoind containers
-docker create --name bitcoind-maindata local:bitcoind-maindata
-docker run -d --name bitcoind-mainnet --volumes-from bitcoind-maindata local:bitcoind-mainnet 
-docker create --name bitcoind-testdata local:bitcoind-testdata
-docker run -d --name bitcoind-testnet --volumes-from bitcoind-testdata local:bitcoind-testnet 
+docker create --name bitcoinxt-maindata local:bitcoinxt-maindata
+docker run -d --name bitcoinxt-mainnet --volumes-from bitcoinxt-maindata local:bitcoinxt-mainnet 
+docker create --name bitcoinxt-testdata local:bitcoinxt-testdata
+docker run -d --name bitcoinxt-testnet --volumes-from bitcoinxt-testdata local:bitcoinxt-testnet 
 
 #build geth images
 docker build --force-rm=true -t "local:geth-data" ~/node_provision/geth-data/
@@ -20,4 +20,4 @@ docker build --force-rm=true -t "local:geth-node" ~/node_provision/geth-node/
 
 #start geth containers
 docker create --name geth-data local:geth-data 
-docker run -d --name geth-node --volumes-from geth-data local:geth-node
+docker run -d --name geth-node -p 8545:8545 -p 30303:30303 --volumes-from geth-data local:geth-node
