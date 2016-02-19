3ce5f7e0bfdf114a1ccbbbe9b607df2df8a87f6d
root <root@HP760-alexykot>
2016-02-19 15:19:31 +0000

    - added geth-testnet - renamed geth-mainnet

--- init_containers.sh
+++ init_containers.sh
@@ -16,8 +16,12 @@ docker run -d --name bitcoin-classic-testnet -p 18333:18333 --volumes-from bitco
 #build geth images
 docker build --force-rm=true -t "local:ubuntu-base" ~/node_provision/ubuntu-base/
 docker build --force-rm=true -t "local:geth-data" ~/node_provision/geth-data/
-docker build --force-rm=true -t "local:geth-node" ~/node_provision/geth-node/
+docker build --force-rm=true -t "local:geth-node-base" ~/node_provision/geth-node-base/
+docker build --force-rm=true -t "local:geth-node-mainnet" ~/node_provision/geth-node-mainnet/
+docker build --force-rm=true -t "local:geth-node-testnet" ~/node_provision/geth-node-testnet/
 
 #start geth containers
-docker create --name geth-data -v /root/.ethereum/keystore:/root/.ethereum/keystore local:geth-data
-docker run -d --name geth-node -p 8545:8545 -p 30303:30303 --volumes-from geth-data local:geth-node
+docker create --name geth-data-mainnet -v /root/.ethereum/keystore-mainnet:/root/.ethereum/keystore local:geth-data
+docker create --name geth-data-testnet -v /root/.ethereum/keystore-testnet:/root/.ethereum/keystore local:geth-data
+docker run -d --name geth-node-mainnet -p 8545:8545   -p 30303:30303 --volumes-from geth-data-mainnet local:geth-node-mainnet 
+docker run -d --name geth-node-testnet -p 18545:18545 -p 40303:40303 --volumes-from geth-data-testnet local:geth-node-testnet
